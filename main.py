7e5a360dce9f42372965a3ae2484c6e9dea6da77
gwend <gwendal.beaumont@gmail.com>
2021-05-27 09:15:55 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+taille = 10000
+distance = np.linspace(0, 200, taille)
+gear = np.zeros(taille)
+torque = np.zeros(taille)
+rpm = []
+delta_d = distance[1]-distance[0]
+v = 110/3.6
+
+for i in distance:
+    coeffs = [3.4, v, -delta_d]
+    roots = np.roots(coeffs)
+    t = roots[1]
+    v = v + 6.8*t
+    rpm.append(v*1000/(60*1.765*9/30*14/23))
+
+print(v*3.6, rpm[-1])
